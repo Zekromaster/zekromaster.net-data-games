0e7ef5ca62e7d93794368029b992b6846cc24f2b
Zekromaster <dev@zekromaster.net>
2023-04-03 23:35:48 +0200

    feat(processing): Switch to alphabetical order for sorting

--- processing/main.py
+++ processing/main.py
@@ -25,8 +25,16 @@ for games_file in games_files:
     games = process_games_text(text, proof_base_path)
     completed_games.extend(games)
 
-# Ordina l'elenco dei giochi completati per data di completamento in ordine crescente
-completed_games.sort(key=lambda x: x["end"])
+# Ordina l'elenco dei giochi completati per slug in ordine crescente
+# Removes "The", "A", "An" from the beginning of a title given as a string
+def remove_articles(title):
+    articles = ["The", "A", "An"]
+    for article in articles:
+        if title.startswith(article):
+            return title[len(article) + 1 :]
+    return title
+
+completed_games.sort(key=lambda x: remove_articles(x["name"]))
 
 # Crea un dizionario con l'elenco dei giochi completati
 toml_data = {"data": completed_games}
